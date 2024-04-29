7c79e26c50bbf64714407e95cfa8a8b087f472fc
piperuao26 <piperuao26@gmail.com>
2024-04-29 15:27:23 -0500

    Finish tuto 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,8 +42,15 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     # Third-party apps start here
     'rest_framework',
+    'rest_framework.authtoken',
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
